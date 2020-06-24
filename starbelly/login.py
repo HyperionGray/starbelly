5f64d2cc2ef718f4774e7c700dc4f8df2b43aab2
Luke <lukemaxwellshouse@gmail.com>
2020-06-24 08:55:49 +0100

    handle missing login credentials

--- starbelly/login.py
+++ starbelly/login.py
@@ -15,11 +15,11 @@ from .downloader import DownloadRequest
 
 
 logger = logging.getLogger(__name__)
-chardet = lambda s: cchardet.detect(s).get('encoding')
+chardet = lambda s: cchardet.detect(s).get("encoding")
 
 
 def get_captcha_image_element(form):
-    '''
+    """
     Return the <img> element in an lxml form that contains the CAPTCHA.
 
     NOTE: This assumes the first image in the form is the CAPTCHA image. If
@@ -29,22 +29,22 @@ def get_captcha_image_element(form):
 
     :param form: An lxml form element.
     :returns: An lxml image element.
-    '''
-    img_el = form.find('.//img')
+    """
+    img_el = form.find(".//img")
     if img_el is None:
-        raise Exception('Cannot locate CAPTCHA image')
+        raise Exception("Cannot locate CAPTCHA image")
     return img_el
 
 
 def select_login_fields(fields):
-    '''
+    """
     Select field having highest probability for class ``field``.
 
     :param dict fields: Nested dictionary containing label probabilities
         for each form element.
     :returns: (username field, password field, captcha field)
     :rtype: tuple
-    '''
+    """
     username_field = None
     username_prob = 0
     password_field = None
@@ -54,14 +54,13 @@ def select_login_fields(fields):
 
     for field_name, labels in fields.items():
         for label, prob in labels.items():
-            if label in ('username', 'username or email') \
-                and prob > username_prob:
+            if label in ("username", "username or email") and prob > username_prob:
                 username_field = field_name
                 username_prob = prob
-            elif label == 'password' and prob > password_prob:
+            elif label == "password" and prob > password_prob:
                 password_field = field_name
                 password_prob = prob
-            elif label == 'captcha' and prob > captcha_prob:
+            elif label == "captcha" and prob > captcha_prob:
                 captcha_field = field_name
                 captcha_prob = prob
 
@@ -69,21 +68,21 @@ def select_login_fields(fields):
 
 
 def select_login_form(forms):
-    '''
+    """
     Select form having highest probability for login class.
 
     :param dict forms: Nested dict containing label probabilities for each
         form.
     :returns: (login form, login meta)
     :rtype: tuple
-    '''
+    """
     login_form = None
     login_meta = None
     login_prob = 0
 
     for form, meta in forms:
-        for type_, prob in meta['form'].items():
-            if type_ == 'login' and prob > login_prob:
+        for type_, prob in meta["form"].items():
+            if type_ == "login" and prob > login_prob:
                 login_form = form
                 login_meta = meta
                 login_prob = prob
@@ -93,72 +92,101 @@ def select_login_form(forms):
 
 class LoginManager:
     def __init__(self, job_id, db, policy, downloader):
-        '''
+        """
         Constructor
 
         :param starbelly.db.LoginDb: A database layer.
-        '''
+        """
         self._job_id = job_id
         self._db = db
         self._policy = policy
         self._downloader = downloader
 
     async def login(self, domain):
-        '''
+        """
         Attempt a login for the given domain.
 
         :param str domain: The domain to log into.
-        '''
-        login = await self._db.get_login(domain)
-        if login is None:
+        """
+        domain_login = await self._db.get_login(domain)
+        if domain_login is None:
             return
 
-        user = random.choice(login['users'])
-        masked_pass = user['password'][:2] + '******'
-        logger.info('Attempting login: domain=%s with user=%s password=%s',
-            domain, user['username'], masked_pass)
-        request = DownloadRequest(frontier_id=None, job_id=self._job_id, method='GET',
-            url=login['login_url'], form_data=None, cost=1.0)
+        # Ensure login has users
+        if not getattr(domain_login, "users", []):
+            logger.warning("No users for login: %s", domain_login)
+            return
+
+        # Select random user from domain_login
+        user = random.choice(domain_login["users"])
+        masked_pass = user["password"][:2] + "******"
+        logger.info(
+            "Attempting login: domain=%s with user=%s password=%s",
+            domain,
+            user["username"],
+            masked_pass,
+        )
+        request = DownloadRequest(
+            frontier_id=None,
+            job_id=self._job_id,
+            method="GET",
+            url=domain_login["login_url"],
+            form_data=None,
+            cost=1.0,
+        )
         response = await self._downloader.download(request)
         if not response.is_success:
-            logger.error('Login aborted: cannot fetch %s', response.url)
+            logger.error("Login aborted: cannot fetch %s", response.url)
             return
         try:
-            action, method, data = await self._get_login_form(response,
-                user['username'], user['password'])
+            action, method, data = await self._get_login_form(
+                response, user["username"], user["password"]
+            )
         except Exception as e:
-            logger.exception('Cannot parse login form: %s', e)
+            logger.exception("Cannot parse login form: %s", e)
             return
-        logger.info('Login action=%s method=%s data=%r', action, method, data)
-        request = DownloadRequest(frontier_id=None, job_id=self._job_id,
-            method=method, url=action, form_data=data, cost=1.0)
+        logger.info("Login action=%s method=%s data=%r", action, method, data)
+        request = DownloadRequest(
+            frontier_id=None,
+            job_id=self._job_id,
+            method=method,
+            url=action,
+            form_data=data,
+            cost=1.0,
+        )
         response = await self._downloader.download(request)
         if not response.is_success:
-            logger.error('Login failed action=%s (see downloader log for'
-                ' details)', action)
+            logger.error(
+                "Login failed action=%s (see downloader log for" " details)", action
+            )
 
     async def _download_captcha_image(self, img_src):
-        '''
+        """
         Download and return a CAPTCHA image.
 
         :param str img_src: The URL to download the image from.
         :rtype bytes:
-        '''
-        logger.info('Downloading CAPTCHA image src=%s', img_src)
-        request = DownloadRequest(frontier_id=None, job_id=None, method='GET',
-            url=img_src, form_data=None, cost=0)
+        """
+        logger.info("Downloading CAPTCHA image src=%s", img_src)
+        request = DownloadRequest(
+            frontier_id=None,
+            job_id=None,
+            method="GET",
+            url=img_src,
+            form_data=None,
+            cost=0,
+        )
         response = await self._downloader.download(request)
 
         if response.status_code == 200 and response.body is not None:
             img_data = response.body
         else:
-            raise Exception('Failed to download CAPTCHA image src={}'.format(
-                img_src))
+            raise Exception("Failed to download CAPTCHA image src={}".format(img_src))
 
         return img_data
 
     async def _get_login_form(self, response, username, password):
-        '''
+        """
         Attempt to extract login form action and form data from a response,
         substituting the provided ``username`` and ``password`` into the
         corresponding fields. Returns the data needed to POST a login request.
@@ -168,22 +196,20 @@ class LoginManager:
         :param str password: The password to log in with.
         :returns: (action, method, fields)
         :rtype: tuple
-        '''
+        """
         _, html = w3lib.encoding.html_to_unicode(
-            response.content_type,
-            response.body,
-            auto_detect_fun=chardet
+            response.content_type, response.body, auto_detect_fun=chardet
         )
 
-        forms = await trio.run_sync_in_worker_thread(partial(
-            formasaurus.extract_forms, html, proba=True))
+        forms = await trio.run_sync_in_worker_thread(
+            partial(formasaurus.extract_forms, html, proba=True)
+        )
         form, meta = select_login_form(forms)
 
         if form is None:
             raise Exception("Can't find login form")
 
-        login_field, password_field, captcha_field = select_login_fields(
-            meta['fields'])
+        login_field, password_field, captcha_field = select_login_fields(meta["fields"])
         if login_field is None or password_field is None:
             raise Exception("Can't find username/password fields")
 
@@ -192,11 +218,13 @@ class LoginManager:
 
         if captcha_field is not None:
             if self._policy.captcha_solver is None:
-                raise Exception('CAPTCHA required for login url={} but there is'
-                    ' no CAPTCHA solver available'.format(response.url))
+                raise Exception(
+                    "CAPTCHA required for login url={} but there is"
+                    " no CAPTCHA solver available".format(response.url)
+                )
 
             img_el = get_captcha_image_element(form)
-            img_src = str(URL(response.url).join(URL(img_el.get('src'))))
+            img_src = str(URL(response.url).join(URL(img_el.get("src"))))
             img_data = await self._download_captcha_image(img_src)
             captcha_text = await self._solve_captcha_asyncio(img_data)
             form.fields[captcha_field] = captcha_text
@@ -206,17 +234,17 @@ class LoginManager:
 
     @trio_asyncio.aio_as_trio
     async def _solve_captcha_asyncio(self, img_data):
-        '''
+        """
         Send an image CAPTCHA to an external solver and return the solution.
         This function uses aiohttp and therefore must run on the asyncio loop.
 
         :param bytes img_data: The CAPTCHA image.
         :rtype: str
-        '''
+        """
         solver = self._policy.captcha_solver
         solution = None
-        task_url = str(URL(solver.service_url).join(URL('createTask')))
-        poll_url = str(URL(solver.service_url).join(URL('getTaskResult')))
+        task_url = str(URL(solver.service_url).join(URL("createTask")))
+        poll_url = str(URL(solver.service_url).join(URL("getTaskResult")))
 
         # This doesn't use the downloader object because this is a third party
         # and is not the subject of our crawl.
@@ -225,31 +253,34 @@ class LoginManager:
             command = solver.get_command(img_data)
             async with session.post(task_url, json=command) as response:
                 result = await response.json()
-                if result['errorId'] != 0:
-                    raise Exception('CAPTCHA API error {}'
-                        .format(result['errorId']))
-                task_id = result['taskId']
-                logger.info('Sent image to CAPTCHA API task_id=%d', task_id)
+                if result["errorId"] != 0:
+                    raise Exception("CAPTCHA API error {}".format(result["errorId"]))
+                task_id = result["taskId"]
+                logger.info("Sent image to CAPTCHA API task_id=%d", task_id)
 
             # Poll for task completion. (Try 6 times.)
             solution = None
             for attempt in range(6):
                 await asyncio.sleep(5)
                 command = {
-                    'clientKey': solver.api_key,
-                    'taskId': task_id,
+                    "clientKey": solver.api_key,
+                    "taskId": task_id,
                 }
-                logger.info('Polling for CAPTCHA solution task_id=%d,'
-                    ' attempt=%d', task_id, attempt+1)
+                logger.info(
+                    "Polling for CAPTCHA solution task_id=%d," " attempt=%d",
+                    task_id,
+                    attempt + 1,
+                )
                 async with session.post(poll_url, json=command) as response:
                     result = await response.json()
-                    if result['errorId'] != 0:
-                        raise Exception('CAPTCHA API error {}'
-                            .format(result['errorId']))
-                    solution = result['solution']['text']
+                    if result["errorId"] != 0:
+                        raise Exception(
+                            "CAPTCHA API error {}".format(result["errorId"])
+                        )
+                    solution = result["solution"]["text"]
                     break
 
         if solution is None:
-            raise Exception('CAPTCHA API never completed task')
+            raise Exception("CAPTCHA API never completed task")
 
         return solution
